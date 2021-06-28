a1ae8bef20489cd8dfc213047b0b68c167c065d2
tejseth <55564043+tejseth@users.noreply.github.com>
2021-06-28 14:48:39 -0400

    Add files via upload

--- tracking-intro.R
+++ tracking-intro.R
@@ -8,9 +8,8 @@ game1 <- read_csv(url("https://raw.githubusercontent.com/tejseth/Big-Data-Bowl-1
 plays_select <- plays %>%
   filter(isSTPlay == "FALSE") %>%
   mutate(pass_or_run = ifelse(is.na(PassLength), "R", "P")) %>%
-  select(gameId, playId, quarter, GameClock, down, yardsToGo, possessionTeam,
-         yardlineSide, yardlineNumber, offenseFormation, personnel.offense,
-         pass_or_run, playDescription)
+  select(gameId, playId, quarter, GameClock, down, yardsToGo, possessionTeam, 
+         yardlineNumber, offenseFormation, personnel.offense, pass_or_run)
 
 plays_select <- plays_select %>%
   mutate(len = nchar(personnel.offense)) %>%
@@ -20,19 +19,23 @@ plays_select <- plays_select %>%
   mutate(num_rb = case_when(
     len == 27 ~ substring(plays_select$personnel.offense, 7, 7),
     len == 22 ~ substring(plays_select$personnel.offense, 7, 7),
-    len == 16 ~ substring(plays_select$personnel.offense, 1, 1)
-  ),
-  num_wr = case_when(
-    len == 27 ~ substring(plays_select$personnel.offense, 24, 24),
+    len == 16 ~ substring(plays_select$personnel.offense, 1, 1)))
+
+plays_select <- plays_select %>%
+  mutate(num_wr = case_when(
+    len == 27 ~ substring(plays_select$personnel.offense, 19, 19),
     len == 22 ~ substring(plays_select$personnel.offense, 19, 19),
-    len == 16 ~ substring(plays_select$off_pers, 13, 13)
-  ))
+    len == 16 ~ substring(plays_select$personnel.offense, 13, 13)))
 
+plays_select <- plays_select %>%
+  mutate(num_te = case_when(
+    len == 27 ~ substring(plays_select$personnel.offense, 7, 7),
+    len == 22 ~ substring(plays_select$personnel.offense, 7, 7),
+    len == 16 ~ substring(plays_select$personnel.offense, 7, 7)))
 
-  mutate(num_rb = substring(plays_select$off_pers, 1, 1),
-         num_te = substring(plays_select$off_pers, 7, 7),
-         num_wr = substring(plays_select$off_pers, 13, 13))
-
+plays_select <- plays_select %>%
+  select(-personnel.offense, -len)
+  
 plays_select$num_rb <- as.numeric(plays_select$num_rb)
 plays_select$num_te <- as.numeric(plays_select$num_te)
 plays_select$num_wr <- as.numeric(plays_select$num_wr)
